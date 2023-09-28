14d19ce945f4e5f4ce2e81fe848fb4f925f50392
Versity1 <vgsicthub@gmail.com>
2023-09-28 16:48:18 +0100

    change the readme

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+function show() {
+    document.getElementById('display').innerHTML='1'
+}
+function show() {
+    document.getElementById('display').innerHTML='2'
+}
+function show() {
+    document.getElementById('display').innerHTML='3'
+}
+function show() {
+    document.getElementById('display').innerHTML='4'
+}
+function show() {
+    document.getElementById('display').innerHTML='5'
+}
+function show() {
+    document.getElementById('display').innerHTML='6'
+}
+function show() {
+    document.getElementById('display').innerHTML='7'
+}
+function show() {
+    document.getElementById('display').innerHTML='8'
+
+function show() {
+    document.getElementById('display').innerHTML='9'
+}
+function show() {
+    document.getElementById('display').innerHTML=''
+}
+function show() {
+    document.getElementById('display').innerHTML=''
+}
+function show() {
+    document.getElementById('display').innerHTML=''
+}
+function show() {
+    document.getElementById('display').innerHTML=''
+}
+function show() {
+    document.getElementById('display').innerHTML=''
+}
+function show(=) {
+    document.getElementById('display').innerHTML=''
+}
